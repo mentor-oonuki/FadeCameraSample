e66ab476778816ab5a0edcbfbcbb64bb3b90a04d
mentor-oonuki <mentor.oonuki@gmail.com>
2017-05-05 21:24:16 +0900

    追加：DOTween追加

---
+++ Assets/Scripts/FadeSample.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.ImageEffects;
+using DG.Tweening;
+
+public class FadeSample : MonoBehaviour {
+
+	private float Intensity;
+	private ScreenOverlay screenOverlay;
+
+
+	void Start()
+	{
+		screenOverlay = gameObject.GetComponent<ScreenOverlay>();
+	}
+
+
+	void Update () {
+		
+	}
+}
